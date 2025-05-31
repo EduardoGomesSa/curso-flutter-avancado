c49545feec166e8a07cc3904c489570841b01d9a
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-05-31 15:16:00 -0300

    test: :test_tube: criando teste por state

---
+++ gerenciamento_estado/test/state_observable_test.dart
@@ -0,0 +1,82 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gerenciamento_estado/classes/counter_state.dart';
+import 'package:gerenciamento_estado/controllers/state_observable.dart';
+
+void main() {
+  group("Should test StateObservable", () {
+    test("Should update state correctly when we increment counter", () {
+      // Arrange
+      final counterState = StateObservable(0);
+      // Act
+      counterState.state++;
+      // Assert
+      expect(counterState.state, 1);
+    });
+  });
+
+  test("Should execute StateObservable callback when we incrementCounter", () {
+    // Arrange
+    bool isCallbackExecuted = false;
+    final counterState = StateObservable(0);
+    // Act
+    void callback() {
+      isCallbackExecuted = true;
+    }
+
+    counterState.addListener(callback);
+
+    counterState.state++;
+    // Assert
+
+    expect(counterState.state, 1);
+    expect(isCallbackExecuted, true);
+  });
+
+  test("Should generate SuccessState when we call (getProducts)", () {
+    //Arrange
+    final ProductController productController = ProductController();
+    expect(productController.state, isA<InitialState>());
+    //Act
+    productController.getProducts();
+    //Assert
+    expect(productController.state, isA<SuccessState>());
+  });
+}
+
+abstract class BaseState {}
+
+class InitialState extends BaseState {}
+
+class LoadingState extends BaseState {}
+
+class SuccessState<T extends Object> extends BaseState {
+  final T data;
+
+  SuccessState({required this.data});
+}
+
+class ErrorState extends BaseState {
+  final String message;
+
+  ErrorState({required this.message});
+}
+
+class Product {
+  final int id;
+  final String name;
+
+  Product({required this.id, required this.name});
+}
+
+class ProductController extends StateObservable<BaseState> {
+  ProductController() : super(InitialState());
+
+  void getProducts() {
+    state = LoadingState();
+
+    state = SuccessState(data: [
+      Product(id: 1, name: "Primeiro produto"),
+      Product(id: 2, name: "Segundo produto"),
+    ]);
+  }
+}
